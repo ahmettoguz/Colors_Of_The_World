1c42a8e74a51d4040e2bcbe232fb3296f4819816
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-05-28 06:51:21 +0300

    Feat: Opacity changes of the elements is performed.

--- assets/script/index.js
+++ assets/script/index.js
@@ -29,9 +29,61 @@ function toggleTheme() {
 }
 
 // main -----------------------------------------------------
+let vision_and_mission_visible = null;
+let screenHeight = window.innerHeight;
+let scrollPosition = window.scrollY;
+
+function scrollElementInvisible() {
+  // make mission and vision invisible
+  setTimeout(() => {
+    $(".card").css("opacity", 0);
+    vision_and_mission_visible = false;
+  }, 500);
+}
+
+function scrollElementVisible() {
+  // mission and vision
+  if (
+    vision_and_mission_visible == false &&
+    scrollPosition / screenHeight > 0.5 &&
+    scrollPosition / screenHeight < 2.5
+  ) {
+    $(".card").animate({ opacity: 1 }, 1000);
+    vision_and_mission_visible = true;
+  }
+}
+
 $(function () {
+  // scroll top
+  $(this).scrollTop(0);
+
+  // make elements invisible for scroll
+  scrollElementInvisible();
+
   // add click event to switch button to change theme
   $(document).on("click", "#theme_Switch", function () {
     toggleTheme();
+  });
+
+  // scroll event
+  window.addEventListener("scroll", function () {
+    screenHeight = window.innerHeight;
+    scrollPosition = window.scrollY;
+
+    // change nav bar constantly
+    if (scrollPosition / screenHeight > 0.2) {
+      if ($("nav").hasClass("opacity-100")) {
+        $("nav").addClass("opacity-75");
+        $("nav").removeClass("opacity-100");
+      }
+    } else {
+      if ($("nav").hasClass("opacity-75")) {
+        $("nav").addClass("opacity-100");
+        $("nav").removeClass("opacity-75");
+      }
+    }
+
+    // make elements visible with scroll
+    scrollElementVisible();
   });
 });
