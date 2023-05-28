67beccb004a59d7b3bb246a2fba50408d2b901cc
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-05-28 07:20:04 +0300

    Feat: Localstorage is storing theme.

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,37 +1,7 @@
-function toggleTheme() {
-  // toggle theme
-  const element = document.body;
-  element.dataset.bsTheme =
-    element.dataset.bsTheme == "light" ? "dark" : "light";
-
-  // get theme mode for other elements
-  const mode = element.dataset.bsTheme;
-
-  // toggle label
-  const label = $(`label.form-check-label[for="theme_Switch"]`);
-  let output = mode == "dark" ? "Dark Mode" : "Light Mode";
-  $(label).html(output);
-
-  // toggle logo
-  src =
-    mode == "dark"
-      ? "./assets/img/logo/logo_dark.png"
-      : "./assets/img/logo/logo_light.png";
-  $("#nav_Logo").attr("src", src);
-
-  // toggle background hexagons
-  output =
-    mode == "dark"
-      ? "./assets/img/hexagon/dark_hexagon_2.jpg"
-      : "./assets/img/hexagon/light_hexagon6.jpg";
-  output = "url('" + output + "')";
-  $(".section").css("background-image", output);
-}
-
-// main -----------------------------------------------------
 let vision_and_mission_visible = null;
 let screenHeight = window.innerHeight;
 let scrollPosition = window.scrollY;
+let theme_Mode = null;
 
 function scrollElementInvisible() {
   // make mission and vision invisible
@@ -53,12 +23,69 @@ function scrollElementVisible() {
   }
 }
 
+function perform_Theme_Change() {
+  // save theme mode
+  localStorage.setItem("theme_Mode", theme_Mode);
+
+  // change overall theme
+  const element = document.body;
+  element.dataset.bsTheme = theme_Mode;
+
+  // change switch
+  if (theme_Mode == "dark") {
+    $("#theme_Switch").prop("checked", true);
+  } else {
+    $("#theme_Switch").prop("checked", false);
+  }
+
+  // toggle label of switch
+  const label = $(`label.form-check-label[for="theme_Switch"]`);
+  let output = theme_Mode == "dark" ? "Dark Mode" : "Light Mode";
+  $(label).html(output);
+
+  // toggle logo
+  src =
+    theme_Mode == "dark"
+      ? "./assets/img/logo/logo_dark.png"
+      : "./assets/img/logo/logo_light.png";
+  $("#nav_Logo").attr("src", src);
+
+  // toggle background hexagons
+  output =
+    theme_Mode == "dark"
+      ? "./assets/img/hexagon/dark_hexagon_2.jpg"
+      : "./assets/img/hexagon/light_hexagon6.jpg";
+  output = "url('" + output + "')";
+  $(".section").css("background-image", output);
+}
+
+function toggleTheme() {
+  // change current theme variable
+  theme_Mode = theme_Mode == "light" ? "dark" : "light";
+
+  perform_Theme_Change();
+}
+
+function initialize_Theme_Mode() {
+  // init light if no previous login
+  if (theme_Mode == null) {
+    theme_Mode = "light";
+  }
+
+  perform_Theme_Change();
+}
+
+// main -----------------------------------------------------
 $(function () {
   // scroll top
   $(this).scrollTop(0);
 
   // make elements invisible for scroll
   scrollElementInvisible();
+
+  // get theme and initialize page
+  theme_Mode = localStorage.getItem("theme_Mode");
+  initialize_Theme_Mode();
 
   // add click event to switch button to change theme
   $(document).on("click", "#theme_Switch", function () {
