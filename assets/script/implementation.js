5ce7aaf0f4d8ff20294f05ef35bda5059746f191
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-06-10 10:25:40 +0300

    Fix: Implementation page scroll animation removed.

--- assets/script/implementation.js
+++ assets/script/implementation.js
@@ -1,23 +1,19 @@
 $(function () {
   $(".summary").css({ top: 0, opacity: 1 });
 
-  $("html, body").animate({ scrollTop: 0 }, 10);
+  $("html, body").scrollTop(0);
 
-  let totalDelay = 200;
+  let totalDelay = 500;
 
   for (let i = 1; i <= 6; i++) {
     let text = ".step_" + i;
     let element = text.valueOf();
+    element = $(element);
 
     setTimeout(() => {
       // display at normal place
-      $(element).css({ left: 0, opacity: 1 });
-
-      // Animate the scrolling to the target element
-      let targetOffset = $(element).offset().top;
-
-      $("html, body").animate({ scrollTop: targetOffset - 300 }, 100);
+      element.css({ left: 0, opacity: 1 });
     }, totalDelay);
-    totalDelay += 500;
+    totalDelay += 200;
   }
 });
